2f06b3fd6395df54853dfefffa494026b1b0de4e
Vatsalya Patel <lnvpatel@gmail.com>
2024-09-09 01:48:31 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,7 +5,13 @@ echo "Installing system dependencies..."
 yum update
 yum install -y nodejs npm
 
+echo "Creating application directory..."
+sudo mkdir -p /var/www/my-new-app
+
 # Install project-specific dependencies
 echo "Installing Node.js dependencies..."
-cd /var/www/my-new-app
+cd /var/www/my-new-app || exit
+npm cache clean --force
+rm -rf node_modules
+
 npm install
