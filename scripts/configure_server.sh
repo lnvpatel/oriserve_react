a2732c002118b819fd21a37d5bfb24fa9c7d1b52
Vatsalya Patel <lnvpatel@gmail.com>
2024-09-09 02:44:35 +0530

    Update configure_server.sh

--- scripts/configure_server.sh
+++ scripts/configure_server.sh
@@ -4,7 +4,7 @@
 echo "Configuring server..."
 
 # Example: Setting permissions
-chown -R www-data:www-data /var/www/my-new-app
+chown -R www-data:www-data /var/www/oriserve_react
 chmod -R 755 /var/www/my-new-app
 
 # Example: Environment variable setup (replace with actual values)
