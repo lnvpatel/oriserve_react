dd9054951bed543f34f147c0aeac77b0696661ba
Vatsalya Patel <lnvpatel@gmail.com>
2024-09-09 03:28:57 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -4,4 +4,4 @@
 echo "Starting application..."
 
 # Example: Using pm2 to manage Node.js application (you may need to install pm2)
-pm2 start /var/www/my-new-app/build/server.js --name my-new-app
+pm2 start /var/www/oriserve_react/build/server.js --name oriserve_react
