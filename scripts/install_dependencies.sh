46017e233ad7c53406add18ac024c997d11bd616
Vatsalya Patel <lnvpatel@gmail.com>
2024-09-09 11:27:13 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # Install necessary system dependencies
-echo "Installing system dependencies..."
-yum update
+echo "Updating system..."
+yum update -y
+
+echo "Installing Node.js and npm..."
 yum install -y nodejs npm
 
 echo "Creating application directory..."
-sudo mkdir -p /var/www/oriserve_react
+mkdir -p /var/www/oriserve_react
 
-# Install project-specific dependencies
-echo "Installing Node.js dependencies..."
-cd /var/www/oriserve_react/builds || exit
+# Navigate to the builds directory
+echo "Navigating to the builds directory..."
+cd /var/www/oriserve_react || { echo "Directory /var/www/oriserve_react does not exist. Exiting."; exit 1; }
+
+# Clean npm cache and install dependencies
+echo "Cleaning npm cache and installing Node.js dependencies..."
 npm cache clean --force
 rm -rf node_modules
-
 npm install
